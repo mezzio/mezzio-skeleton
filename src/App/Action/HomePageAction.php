02d106d70af4e64394530587387621898855037c
Geert Eltink <xtreamwayz@gmail.com>
2015-09-04 23:28:53 +0200

    Show which router is used

--- src/App/Action/HomePageAction.php
+++ src/App/Action/HomePageAction.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
+use Zend\Expressive\Router\RouterInterface;
 use Zend\Expressive\Template\TemplateInterface;
 
 class HomePageAction extends AbstractAction
@@ -19,8 +20,25 @@ class HomePageAction extends AbstractAction
             ]);
         }
 
+        $data = [];
+
+        $router = $this->getContainer()->get(RouterInterface::class);
+        if ($router instanceof Zend\Expressive\Router\AuraRouter) {
+            $data['routerDocsUrl'] = 'https://zend-expressive.readthedocs.org/en/latest/router/aura/';
+            $data['routerName'] = 'Aura.Router';
+            $data['routerExtUrl'] = 'http://auraphp.com/packages/Aura.Router/';
+        } elseif ($router instanceof Zend\Expressive\Router\FastRouteRouter) {
+            $data['routerDocsUrl'] = 'https://zend-expressive.readthedocs.org/en/latest/router/fast-route/';
+            $data['routerName'] = 'FastRoute';
+            $data['routerExtUrl'] = 'https://github.com/nikic/FastRoute';
+        } elseif ($router instanceof Zend\Expressive\Router\Zf2Router) {
+            $data['routerDocsUrl'] = 'https://zend-expressive.readthedocs.org/en/latest/router/zf2/';
+            $data['routerName'] = 'ZF2 Router';
+            $data['routerExtUrl'] = 'http://framework.zend.com/manual/current/en/modules/zend.mvc.routing.html';
+        }
+
         return new HtmlResponse($this->getRenderer()->render('app::home-page', [
-            'entry' => 'first entry'
+            $data
         ]));
     }
 }
