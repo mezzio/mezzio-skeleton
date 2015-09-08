9058f9cebbb24cf8ae83bd8bc76c2943218c4df3
Geert Eltink <xtreamwayz@gmail.com>
2015-09-08 10:14:16 +0200

    Remove StrictPhp loader

--- src/Zend/Expressive/Composer/Resources/config/zend-servicemanager-container.php
+++ src/Zend/Expressive/Composer/Resources/config/zend-servicemanager-container.php
@@ -8,10 +8,6 @@ foreach (glob('config/autoload/{{,*.}global,{,*.}local}.php', GLOB_BRACE) as $fi
     $config = array_replace_recursive($config, include $file);
 }
 
-if (isset($config['strict_php'])) {
-    StrictPhp\StrictPhpKernel::getInstance()->init($config['strict_php']);
-}
-
 $container = new ServiceManager(new Config($config['dependencies']));
 $container->setService('Config', $config);
 
