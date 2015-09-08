f749f7f2e9cf75f836285e5c344ab4a435041446
Geert Eltink <xtreamwayz@gmail.com>
2015-09-08 15:22:20 +0200

    Update templates

--- src/Action/HomePageAction.php
+++ src/Action/HomePageAction.php
@@ -7,7 +7,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
 use Zend\Expressive\Router;
-use Zend\Expressive\Template\TemplateInterface;
+use Zend\Expressive\Template;
 
 class HomePageAction
 {
@@ -15,7 +15,7 @@ class HomePageAction
 
     private $template;
 
-    public function __construct(Router\RouterInterface $router, TemplateInterface $template = null)
+    public function __construct(Router\RouterInterface $router, Template\TemplateInterface $template = null)
     {
         $this->router   = $router;
         $this->template = $template;
@@ -23,27 +23,37 @@ class HomePageAction
 
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
     {
-        if (!$this->template) {
-            return new JsonResponse([
-                'welcome' => 'Congratulations! You have installed the zend-expressive skeleton application',
-                'docs'    => 'https://zend-expressive.readthedocs.org/en/latest/'
-            ]);
-        }
-
         $data = [];
 
         if ($this->router instanceof Router\AuraRouter) {
-            $data['routerDocsUrl'] = 'https://zend-expressive.readthedocs.org/en/latest/router/aura/';
-            $data['routerName']    = 'Aura.Router';
-            $data['routerExtUrl']  = 'http://auraphp.com/packages/Aura.Router/';
+            $data['routerName'] = 'Aura.Router';
+            $data['routerDocs'] = 'http://auraphp.com/packages/Aura.Router/';
         } elseif ($this->router instanceof Router\FastRouteRouter) {
-            $data['routerDocsUrl'] = 'https://zend-expressive.readthedocs.org/en/latest/router/fast-route/';
-            $data['routerName']    = 'FastRoute';
-            $data['routerExtUrl']  = 'https://github.com/nikic/FastRoute';
+            $data['routerName'] = 'FastRoute';
+            $data['routerDocs'] = 'https://github.com/nikic/FastRoute';
         } elseif ($this->router instanceof Router\Zf2Router) {
-            $data['routerDocsUrl'] = 'https://zend-expressive.readthedocs.org/en/latest/router/zf2/';
-            $data['routerName']    = 'ZF2 Router';
-            $data['routerExtUrl']  = 'http://framework.zend.com/manual/current/en/modules/zend.mvc.routing.html';
+            $data['routerName'] = 'ZF2 Router';
+            $data['routerDocs'] = 'http://framework.zend.com/manual/current/en/modules/zend.mvc.routing.html';
+        }
+
+        if ($this->template instanceof Template\Plates) {
+            $data['templateName'] = 'Plates';
+            $data['templateDocs'] = 'http://platesphp.com/';
+        } elseif ($this->template instanceof Template\Twig) {
+            $data['templateName'] = 'Twig';
+            $data['templateDocs'] = 'http://twig.sensiolabs.org/documentation';
+        } elseif ($this->template instanceof Template\ZendView) {
+            $data['templateName'] = 'Zend View';
+            $data['templateDocs'] = 'http://framework.zend.com/manual/current/en/modules/zend.view.quick-start.html';
+        }
+
+        if (!$this->template) {
+            return new JsonResponse([
+                'welcome'  => 'Congratulations! You have installed the zend-expressive skeleton application.',
+                'docs'     => 'https://zend-expressive.readthedocs.org/en/latest/',
+                'router'   => sprintf('You installed %s', $data['routerName']),
+                'template' => sprintf('You installed %s template engine.', $data['templateName'])
+            ]);
         }
 
         return new HtmlResponse($this->template->render('app::home-page', $data));
