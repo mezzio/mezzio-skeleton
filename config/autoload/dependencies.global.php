e4c253cdf144ba977a0aaa584980d344faca9332
Renan de Lima <renandelima@gmail.com>
2015-10-20 11:06:58 +0200

    move actions config from dependencies.global.php to routes.global.php

--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -3,11 +3,8 @@
 return [
     'dependencies' => [
         'invokables' => [
-            App\Action\PingAction::class => App\Action\PingAction::class,
         ],
         'factories' => [
-            App\Action\HomePageAction::class => App\Action\HomePageFactory::class,
-            Zend\Expressive\Application::class => Zend\Expressive\Container\ApplicationFactory::class,
         ]
     ]
 ];
--- src/Composer/Resources/config/routes-zf2-router.php
+++ src/Composer/Resources/config/routes-zf2-router.php
@@ -4,6 +4,11 @@ return [
     'dependencies' => [
         'invokables' => [
             Zend\Expressive\Router\RouterInterface::class => Zend\Expressive\Router\ZendRouter::class,
+            App\Action\PingAction::class => App\Action\PingAction::class,
+        ],
+        'factories' => [
+            App\Action\HomePageAction::class => App\Action\HomePageFactory::class,
+            Zend\Expressive\Application::class => Zend\Expressive\Container\ApplicationFactory::class,
         ],
     ],
 
