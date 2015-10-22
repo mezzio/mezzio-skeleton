f7aa29d012f55c32e2dfca473edb836eb761767b
Renan de Lima <renandelima@gmail.com>
2015-10-22 11:26:45 +0200

    add comments to the dependencies.global.php
    https://github.com/zendframework/zend-expressive-skeleton/pull/11#discus
    sion_r42595261

--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -1,9 +1,17 @@
 <?php
 
 return [
+    // Provides services for whole application
+    // It's recommend using fully-qualified class names whenever possible as
+    // service names
     'dependencies' => [
+        // Use 'invokables' for constructor-less services
+        // It tells to the container what class to instantiate when a particular
+        // service is requested
         'invokables' => [
+            //Fully\Qualified\InterfaceName::class => Fully\Qualified\ClassName::class,
         ],
+        // Use 'factories' for services provided by callbacks
         'factories' => [
             Zend\Expressive\Application::class => Zend\Expressive\Container\ApplicationFactory::class,
         ]
