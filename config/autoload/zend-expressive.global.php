dddd13ab798983a97eeda363426884f845ffbc04
Matthew Weier O'Phinney <matthew@zend.com>
2017-01-09 17:04:45 -0600

    Edits to comments in expressive configuration

--- config/autoload/zend-expressive.global.php
+++ config/autoload/zend-expressive.global.php
@@ -7,11 +7,15 @@ return [
     'config_cache_enabled' => false,
 
     'zend-expressive' => [
-        // Enable the new error handling.
+        // Enable exception-based error handling via standard middleware.
         'raise_throwables' => true,
+
         // Enable programmatic pipeline: Any `middleware_pipeline` or `routes`
         // configuration will be ignored when creating the `Application` instance.
         'programmatic_pipeline' => true,
+
+        // Provide templates for the error handling middleware to use when
+        // generating responses.
         'error_handler' => [
             'template_404'   => 'error::404',
             'template_error' => 'error::error',
