552bcffe1b485380bb990582573cd96e4c745163
Geert Eltink <xtreamwayz@gmail.com>
2017-01-10 19:53:26 +0100

    Add docblocks to explain what the ConfigProvider is used for

--- src/App/src/ConfigProvider.php
+++ src/App/src/ConfigProvider.php
@@ -2,8 +2,21 @@
 
 namespace App;
 
+/**
+ * The configuration provider for the App module
+ *
+ * @see https://docs.zendframework.com/zend-component-installer/
+ */
 class ConfigProvider
 {
+    /**
+     * Returns the configuration array
+     *
+     * To add a bit of a structure, each section is defined in a separate
+     * method which returns an array with its configuration.
+     *
+     * @return array
+     */
     public function __invoke()
     {
         return [
@@ -12,6 +25,11 @@ class ConfigProvider
         ];
     }
 
+    /**
+     * Returns the container dependencies
+     *
+     * @return array
+     */
     public function getDependencies()
     {
         return [
@@ -24,6 +42,11 @@ class ConfigProvider
         ];
     }
 
+    /**
+     * Returns the templates configuration
+     *
+     * @return array
+     */
     public function getTemplates()
     {
         return [
