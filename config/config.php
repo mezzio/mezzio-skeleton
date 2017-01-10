1800b87d68cfcaba512cba27edfb9ba67b3f73fc
Geert Eltink <xtreamwayz@gmail.com>
2017-01-10 19:34:26 +0100

    Reduce indentation and clarify how and where to trigger caching

--- config/config.php
+++ config/config.php
@@ -3,33 +3,31 @@
 use Zend\ConfigAggregator\ConfigAggregator;
 use Zend\ConfigAggregator\PhpFileProvider;
 
+// To enable or disable caching, set the `ConfigAggregator::ENABLE_CACHE` boolean in
+// `config/autoload/local.php`.
 $cacheConfig = [
     'config_cache_path' => 'data/config-cache.php',
 ];
 
-$aggregator = new ConfigAggregator(
-    [
-        //Include cache config for zf-development-mode
-        function () use ($cacheConfig) {
-            return $cacheConfig;
-        },
+$aggregator = new ConfigAggregator([
+    //Include cache config for zf-development-mode
+    function () use ($cacheConfig) {
+        return $cacheConfig;
+    },
 
-        // Load module config
-        App\ConfigProvider::class,
+    // Load module config
+    App\ConfigProvider::class,
 
-        // Load application config in a pre-defined order in such a way that local settings
-        // overwrite global settings. (Loaded as first to last):
-        //   - `global.php`
-        //   - `*.global.php`
-        //   - `local.php`
-        //   - `*.local.php`
-        new PhpFileProvider('config/autoload/{{,*.}global,{,*.}local}.php'),
+    // Load application config in a pre-defined order in such a way that local settings
+    // overwrite global settings. (Loaded as first to last):
+    //   - `global.php`
+    //   - `*.global.php`
+    //   - `local.php`
+    //   - `*.local.php`
+    new PhpFileProvider('config/autoload/{{,*.}global,{,*.}local}.php'),
 
-        // Load development config if it exists
-        new PhpFileProvider('config/development.config.php'),
-    ],
-    // Cached config file
-    $cacheConfig['config_cache_path']
-);
+    // Load development config if it exists
+    new PhpFileProvider('config/development.config.php'),
+], $cacheConfig['config_cache_path']);
 
 return $aggregator->getMergedConfig();
