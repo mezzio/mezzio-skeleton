fb754a847065647c48cb088091ed91d7c4c530a3
Matthew Weier O'Phinney <matthew@zend.com>
2017-01-11 17:28:10 -0600

    Enable configuration caching by default.

--- config/autoload/zend-expressive.global.php
+++ config/autoload/zend-expressive.global.php
@@ -1,6 +1,15 @@
 <?php
 
+use Zend\ConfigAggregator\ConfigAggregator;
+
 return [
+    // Toggle the configuration cache. Set this to boolean false, or remove the
+    // directive, to disable configuration caching. Toggling development mode
+    // will also disable it by default; clear the configuration cache using
+    // `composer clear-config-cache`.
+    ConfigAggregator::ENABLE_CACHE => true,
+
+    // Enable debugging; typically used to provide debugging information within templates.
     'debug' => false,
 
     'zend-expressive' => [
