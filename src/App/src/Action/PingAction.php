c5af94e6b32d7871296219fb427ac12e321906ce
Matthew Weier O'Phinney <matthew@zend.com>
2017-01-31 13:37:21 -0600

    Remove unused import statement from PingAction

--- src/App/src/Action/PingAction.php
+++ src/App/src/Action/PingAction.php
@@ -5,7 +5,6 @@ namespace App\Action;
 use Interop\Http\ServerMiddleware\DelegateInterface;
 use Interop\Http\ServerMiddleware\MiddlewareInterface as ServerMiddlewareInterface;
 use Zend\Diactoros\Response\JsonResponse;
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class PingAction implements ServerMiddlewareInterface
