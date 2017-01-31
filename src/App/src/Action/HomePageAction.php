6bf2683f85f5966efa0b9241461f4b46ee28c24f
Matthew Weier O'Phinney <matthew@zend.com>
2017-01-31 13:36:57 -0600

    Remove unused import statement from HomePageAction

--- src/App/src/Action/HomePageAction.php
+++ src/App/src/Action/HomePageAction.php
@@ -4,7 +4,6 @@ namespace App\Action;
 
 use Interop\Http\ServerMiddleware\DelegateInterface;
 use Interop\Http\ServerMiddleware\MiddlewareInterface as ServerMiddlewareInterface;
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
