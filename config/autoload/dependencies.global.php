da6c41242c872b032b1979bfe554fd23384fbe47
Matthew Weier O'Phinney <matthew@zend.com>
2017-02-09 10:51:48 -0600

    Wire the NotFoundDelegate and DefaultDelegate services
    - Updates to current develop branch of Expressive.
    - Adds mapping of NotFoundDelegate to its factory.
    - Aliases DefaultDelegate to NotFoundDelegate service.

--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -2,6 +2,7 @@
 
 use Zend\Expressive\Application;
 use Zend\Expressive\Container;
+use Zend\Expressive\Delegate;
 use Zend\Expressive\Helper;
 use Zend\Expressive\Middleware;
 
@@ -10,6 +11,11 @@ return [
     // We recommend using fully-qualified class names whenever possible as
     // service names.
     'dependencies' => [
+        // Use 'aliases' to alias a service name to another service. The
+        // key is the alias name, the value is the service to which it points.
+        'aliases' => [
+            Delegate\DefaultDelegate::class => Delegate\NotFoundDelegate::class,
+        ],
         // Use 'invokables' for constructor-less services, or services that do
         // not require arguments to the constructor. Map a service name to the
         // class name.
@@ -20,6 +26,7 @@ return [
         // Use 'factories' for services provided by callbacks/factory classes.
         'factories'  => [
             Application::class                => Container\ApplicationFactory::class,
+            Delegate\NotFoundDelegate::class  => Container\NotFoundDelegateFactory::class,
             Helper\ServerUrlMiddleware::class => Helper\ServerUrlMiddlewareFactory::class,
             Helper\UrlHelper::class           => Helper\UrlHelperFactory::class,
             Helper\UrlHelperMiddleware::class => Helper\UrlHelperMiddlewareFactory::class,
