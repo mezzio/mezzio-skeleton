71e2ac5ef6617aa4156a2abf91e83ce93aa217d8
Geert Eltink <xtreamwayz@gmail.com>
2018-02-06 21:32:39 +0100

    Get zend-expressive-helper config from its ConfigProvider

--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Zend\Expressive\Application;
 use Zend\Expressive\Container;
 use Zend\Expressive\Delegate;
-use Zend\Expressive\Helper;
 use Zend\Expressive\Middleware;
 
 return [
@@ -23,15 +22,11 @@ return [
         // class name.
         'invokables' => [
             // Fully\Qualified\InterfaceName::class => Fully\Qualified\ClassName::class,
-            Helper\ServerUrlHelper::class => Helper\ServerUrlHelper::class,
         ],
         // Use 'factories' for services provided by callbacks/factory classes.
         'factories'  => [
             Application::class                => Container\ApplicationFactory::class,
             Delegate\NotFoundDelegate::class  => Container\NotFoundDelegateFactory::class,
-            Helper\ServerUrlMiddleware::class => Helper\ServerUrlMiddlewareFactory::class,
-            Helper\UrlHelper::class           => Helper\UrlHelperFactory::class,
-            Helper\UrlHelperMiddleware::class => Helper\UrlHelperMiddlewareFactory::class,
 
             Zend\Stratigility\Middleware\ErrorHandler::class => Container\ErrorHandlerFactory::class,
             Middleware\ErrorResponseGenerator::class         => Container\ErrorResponseGeneratorFactory::class,
