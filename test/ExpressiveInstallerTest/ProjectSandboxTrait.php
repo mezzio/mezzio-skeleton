715e749482872aa72d0b9be7260d7f0369167a0b
Geert Eltink <xtreamwayz@gmail.com>
2018-02-15 07:12:54 +0100

    Update copyright date

--- test/ExpressiveInstallerTest/ProjectSandboxTrait.php
+++ test/ExpressiveInstallerTest/ProjectSandboxTrait.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-expressive-installer for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2017-2018 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive-installer/blob/master/LICENSE.md New BSD License
  */
 
