b6cb3afe6968377430c5c82aca845ecfca6c00f4
Witold Wasiczko <witold@wasiczko.pl>
2018-02-26 21:05:40 +0100

    Simplify build servicemanager

--- src/ExpressiveInstaller/Resources/config/container-zend-servicemanager.php
+++ src/ExpressiveInstaller/Resources/config/container-zend-servicemanager.php
@@ -2,17 +2,13 @@
 
 declare(strict_types=1);
 
-use Zend\ServiceManager\Config;
 use Zend\ServiceManager\ServiceManager;
 
 // Load configuration
 $config = require __DIR__ . '/config.php';
 
+$dependencies = $config['dependencies'];
+$dependencies['services']['config'] = $config;
+
 // Build container
-$container = new ServiceManager();
-(new Config($config['dependencies']))->configureServiceManager($container);
-
-// Inject config
-$container->setService('config', $config);
-
-return $container;
+return new ServiceManager($dependencies);
