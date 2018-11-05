dfc63deae696cfab03c1dffe4827353f550681d6
Geert Eltink <xtreamwayz@gmail.com>
2018-11-05 09:24:01 +0100

    Update coding style

--- src/App/src/Handler/HomePageHandler.php
+++ src/App/src/Handler/HomePageHandler.php
@@ -11,26 +11,29 @@ use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\JsonResponse;
 use Zend\Expressive\Plates\PlatesRenderer;
 use Zend\Expressive\Router;
-use Zend\Expressive\Template;
+use Zend\Expressive\Template\TemplateRendererInterface;
 use Zend\Expressive\Twig\TwigRenderer;
 use Zend\Expressive\ZendView\ZendViewRenderer;
 
 class HomePageHandler implements RequestHandlerInterface
 {
+    /** @var string */
     private $containerName;
 
+    /** @var Router\RouterInterface */
     private $router;
 
+    /** @var null|TemplateRendererInterface */
     private $template;
 
     public function __construct(
+        string $containerName,
         Router\RouterInterface $router,
-        Template\TemplateRendererInterface $template = null,
-        string $containerName
+        ?TemplateRendererInterface $template = null
     ) {
+        $this->containerName = $containerName;
         $this->router        = $router;
         $this->template      = $template;
-        $this->containerName = $containerName;
     }
 
     public function handle(ServerRequestInterface $request) : ResponseInterface
