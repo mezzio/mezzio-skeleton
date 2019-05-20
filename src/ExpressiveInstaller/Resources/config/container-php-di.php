d42fdde08363478dcaf03216ec975e50ac3af364
Aleksei Khudiakov <aleksey@xerkus.pro>
2019-05-21 05:47:37 +1000

    Change call_user_func to IIFE

--- src/ExpressiveInstaller/Resources/config/container-php-di.php
+++ src/ExpressiveInstaller/Resources/config/container-php-di.php
@@ -4,11 +4,11 @@ use Elie\PHPDI\Config\Config;
 use Elie\PHPDI\Config\ContainerFactory;
 
 // Protect variables from global scope
-return call_user_func(function () {
+return (function () {
 
     $config = require __DIR__ . '/config.php';
 
     $factory = new ContainerFactory();
 
     return $factory(new Config($config));
-});
+})();
