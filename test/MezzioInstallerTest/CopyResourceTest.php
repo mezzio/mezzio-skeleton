bb055626b7f20b20eb8c0ed3c39e211fc9cbaf9e
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:47:16 -0600

    Rewriting as Laminas Project package

---
+++ test/MezzioInstallerTest/CopyResourceTest.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * @see       https://github.com/mezzio/mezzio-skeleton for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-skeleton/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-skeleton/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace MezzioInstallerTest;
+
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+
+class CopyResourceTest extends OptionalPackagesTestCase
+{
+    /**
+     * @var vfsStreamDirectory
+     */
+    private $project;
+
+    /**
+     * @var string URL of project root
+     */
+    private $projectRoot;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->project = vfsStream::setup('project-root');
+        $this->projectRoot = vfsStream::url('project-root');
+    }
+
+    public function testTargetFileDoesNotExist()
+    {
+        $this->assertFalse($this->project->hasChild('data'));
+        $this->assertFalse($this->project->hasChild('data/target.php'));
+    }
+
+    public function testResourceIsCopiedIfItDoesNotExist()
+    {
+        $installer = $this->createOptionalPackages($this->projectRoot);
+
+        $installer->copyResource('config.php', 'data/target.php');
+
+        $this->assertTrue($this->project->hasChild('data'));
+        $this->assertTrue($this->project->hasChild('data/target.php'));
+        $this->assertFileEquals(
+            $this->packageRoot . '/src/MezzioInstaller/config.php',
+            vfsStream::url('project-root/data/target.php')
+        );
+    }
+
+    public function testResourceIsNotCopiedIfItExists()
+    {
+        // Create default test file
+        vfsStream::newFile('data/target.php')
+            ->at($this->project)
+            ->setContent('TEST');
+
+        $this->assertTrue($this->project->hasChild('data/target.php'));
+
+        // Copy file (should not copy file)
+        $installer = $this->createOptionalPackages($this->projectRoot);
+        $installer->copyResource('config.php', 'data/target.php');
+
+        $this->assertTrue($this->project->hasChild('data/target.php'));
+        $this->assertEquals(
+            'TEST',
+            file_get_contents(vfsStream::url('project-root/data/target.php'))
+        );
+    }
+}
