6587e40bb418a09075f8fbab174dbf071b2dbdf3
Michał Bundyra <contact@webimpress.com>
2020-04-21 18:34:49 +0100

    Feature: adds support for Aura.Di 4.0
    Change laminas-auradi-config to 2.0
    
    Signed-off-by: Michał Bundyra <contact@webimpress.com>

--- test/MezzioInstallerTest/HomePageResponseTest.php
+++ test/MezzioInstallerTest/HomePageResponseTest.php
@@ -69,7 +69,7 @@ class HomePageResponseTest extends OptionalPackagesTestCase
     private $expectedContainerAttributes = [
         AuraDiContainer::class => [
             'containerName' => 'Aura.Di',
-            'containerDocs' => 'http://auraphp.com/packages/3.x/Di/',
+            'containerDocs' => 'http://auraphp.com/packages/4.x/Di/',
         ],
         PimpleContainer::class => [
             'containerName' => 'Pimple',
