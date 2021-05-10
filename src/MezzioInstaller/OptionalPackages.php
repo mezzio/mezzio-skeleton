b780bc27c8fbd704cab90c655ea6052829b4a4d7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:03:04 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/MezzioInstaller/OptionalPackages.php
+++ src/MezzioInstaller/OptionalPackages.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/mezzio/mezzio-skeleton for the canonical source repository
- * @copyright https://github.com/mezzio/mezzio-skeleton/blob/master/COPYRIGHT.md
- * @license   https://github.com/mezzio/mezzio-skeleton/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace MezzioInstaller;
