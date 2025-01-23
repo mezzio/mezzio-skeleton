0283f71b46dec6ee5c5c3294ddadaf866d7f69d2
Florin Bidirean <bidi@dotkernel.com>
2025-01-23 12:55:30 +0200

    Removed duplicate ConfigProvider from config/config.php
    Signed-off-by: Florin Bidirean <bidi@dotkernel.com>

--- config/config.php
+++ config/config.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Laminas\ConfigAggregator\ArrayProvider;
 use Laminas\ConfigAggregator\ConfigAggregator;
 use Laminas\ConfigAggregator\PhpFileProvider;
-use Mezzio\Helper\ConfigProvider;
 
 // To enable or disable caching, set the `ConfigAggregator::ENABLE_CACHE` boolean in
 // `config/autoload/local.php`.
@@ -16,7 +15,6 @@ $cacheConfig = [
 $aggregator = new ConfigAggregator([
     // Include cache configuration
     new ArrayProvider($cacheConfig),
-    ConfigProvider::class,
     \Mezzio\ConfigProvider::class,
     \Mezzio\Router\ConfigProvider::class,
     \Laminas\Diactoros\ConfigProvider::class,
